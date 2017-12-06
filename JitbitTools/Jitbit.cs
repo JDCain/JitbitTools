bc0501b9a9e01bb84c66b6797fdac50fef659b15
J.D. Cain <jd.cain.jr@gmail.com>
2017-12-06 09:39:40 -0500

    Init

---
+++ JitbitTools/Jitbit.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using JitbitTools.Models;
+using Newtonsoft.Json;
+
+namespace JitbitTools
+{
+    public class Jitbit
+    {
+        private readonly byte[] _credentials;
+        private readonly string _baseUrl;
+        public Jitbit(byte[] credentials, string baseUrl)
+        {
+            _credentials = credentials;
+            _baseUrl = baseUrl;
+        }
+
+        public async Task<IEnumerable<Ticket>> GetTicketsByParameters(Dictionary<string, string> parameters)
+        {
+
+            var url = $"{_baseUrl}/api/Tickets";
+            var response = await Post(parameters, url);
+            var jsonString = await ProcessResponse(response);
+            try
+            {
+                return JsonConvert.DeserializeObject<List<Ticket>>(jsonString);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<CustomField>> GetCustomFields(Ticket ticket)
+        {
+            var url = $"{_baseUrl}/api/TicketCustomFields?id={ticket.IssueID}";
+            var response = await Get(url);
+            var jsonString = await ProcessResponse(response);
+            try
+            {
+                return JsonConvert.DeserializeObject<List<CustomField>>(jsonString);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Ticket>> GetTickets(int catagory, bool getCustomFields = false, string mode = "unclosed")
+        {
+            var offset = 0;
+            var allTickets = new List<Ticket>();
+            List<Ticket> ticketSet;
+            do
+            {
+                ticketSet = await GetTicketsByParameters(new Dictionary<string, string>
+                {
+                    {"categoryId", catagory.ToString()},
+                    {"count", "100"},
+                    {"offset", $"{offset}"},
+                    {"mode", mode}
+                }) as List<Ticket>;
+                allTickets.AddRange(ticketSet?.ToList());
+
+                offset += 100;
+            } while (ticketSet?.Count == 100);
+
+            if (getCustomFields)
+            {
+                foreach (var ticket in allTickets)
+                {
+                    Console.WriteLine($"Getting Custom fields for {ticket.IssueID}");
+                    ticket.CustomFields = await GetCustomFields(ticket);
+                }
+            }
+            return allTickets;
+        }
+
+        public async Task CloseTicket(Ticket ticket)
+        {
+            var url = $"{_baseUrl}API/UpdateTicket";
+            var parameters = new Dictionary<string, string> { { "id", ticket.IssueID.ToString() }, { "statusID", 3.ToString() } };
+            var response = await Post(parameters, url);
+        }
+
+        public async Task Comment(Ticket ticket, string comment, bool techOnly = true)
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                {"id", ticket.IssueID.ToString()},
+                {"body", comment},
+                {"forTechsOnly", techOnly.ToString()}
+            };
+            var url = $"{_baseUrl}/API/comment";
+            var response = await Post(parameters, url);
+        }
+
+        public async Task WriteCustomField(Ticket ticket, CustomField field)
+        {
+            var url = $"{_baseUrl}/API/SetCustomField";
+            var parameters = new Dictionary<string, string>
+            {
+                {"ticketid", ticket.IssueID.ToString()},
+                {"fieldId", field.FieldID.ToString()},
+                {"value", field.Value}
+            };
+            await Post(parameters, url);
+        }
+
+        private async Task<string> ProcessResponse(HttpResponseMessage responseMessage)
+        {
+            string result = null;
+            if (responseMessage.StatusCode == HttpStatusCode.OK)
+            {
+                result = await responseMessage.Content.ReadAsStringAsync();
+            }
+            return result;
+        }
+
+        private async Task<HttpResponseMessage> Post(Dictionary<string, string> parameters, string url)
+        {
+            HttpResponseMessage responseMessage;
+            using (var httpClient = new HttpClient())
+            {
+                var encodedContent = new FormUrlEncodedContent(parameters);
+                SetCreds(httpClient);
+                responseMessage = await httpClient.PostAsync(url, encodedContent).ConfigureAwait(false);
+                if (responseMessage.StatusCode == (HttpStatusCode)429)
+                {
+                    await Task.Delay(60000);
+                    responseMessage = await Post(parameters, url);
+                }
+            }
+            return responseMessage;
+        }
+
+        private async Task<HttpResponseMessage> Get(string url)
+        {
+            HttpResponseMessage responseMessage;
+            using (var httpClient = new HttpClient())
+            {
+                SetCreds(httpClient);
+                responseMessage = await httpClient.GetAsync(url).ConfigureAwait(false);
+                if (responseMessage.StatusCode == (HttpStatusCode)429)
+                {
+                    await Task.Delay(60000);
+                    responseMessage = await Get(url);
+                }
+            }
+            return responseMessage;
+        }
+
+        private void SetCreds(HttpClient httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Basic", Convert.ToBase64String(_credentials));
+        }
+    }
+}
---
+++ JitbitTools/Models/Ticket.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace JitbitTools.Models
+{
+    public class Ticket
+    {
+        public int? IssueID { get; set; }
+        public int? Priority { get; set; }
+        public int? StatusID { get; set; }
+        public DateTime? IssueDate { get; set; }
+        public string Subject { get; set; }
+        public string Status { get; set; }
+        public bool? UpdatedByUser { get; set; }
+        public bool? UpdatedByPerformer { get; set; }
+        public int? CategoryID { get; set; }
+        public string UserName { get; set; }
+        public string Technician { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string TechFirstName { get; set; }
+        public string TechLastName { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public bool UpdatedForTechView { get; set; }
+        public int? UserID { get; set; }
+        public int? CompanyID { get; set; }
+        public string CompanyName { get; set; }
+        public int? AssignedToUserID { get; set; }
+        public DateTime? ResolvedDate { get; set; }
+        public int? SectionID { get; set; }
+        public string Category { get; set; }
+        public string Origin { get; set; }
+        public string ViewingTechs { get; set; }
+        public IEnumerable<CustomField> CustomFields { get; set; }
+    }
+}
